bc8dabdd99f102fa90c6e6c83e4dba9c4b1a3e56
Aldexyn <156871338+Aldexyn@users.noreply.github.com>
2025-05-12 21:01:04 +0900

    updates

--- mileage/mileage.js
+++ mileage/mileage.js
@@ -2,14 +2,14 @@ const apply = document.getElementById('apply');
 const warning = document.getElementById('warning');
 const use = document.getElementById('use');
 const price = document.getElementById('price');
-const earn = document.getElementById('earn');
 apply.addEventListener('click', function () {
     const mileage = parseInt(use.value);
     if (mileage > 1800) {
         warning.hidden = false;
-        use.value = 0;
+        use.value = "";
+        price.textContent = (6000).toLocaleString();
     } else {
+        warning.hidden = true;
         price.textContent = (6000 - mileage).toLocaleString();
-        earn.textContent = ((6000 - mileage) * 0.25).toLocaleString();
     }
 })
